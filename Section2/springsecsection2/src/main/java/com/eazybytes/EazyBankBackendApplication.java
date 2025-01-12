c3aac6e04b3a9ac5c330133be57f5c3237f4d3c6
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-01-13 00:16:01 +0530

    created different controllers and SecurityFilterChain Bean

--- Section2/springsecsection2/src/main/java/com/eazybytes/EazyBankBackendApplication.java
+++ Section2/springsecsection2/src/main/java/com/eazybytes/EazyBankBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Springsecsection2Application {
+public class EazyBankBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Springsecsection2Application.class, args);
+		SpringApplication.run(EazyBankBackendApplication.class, args);
 	}
 
 }
