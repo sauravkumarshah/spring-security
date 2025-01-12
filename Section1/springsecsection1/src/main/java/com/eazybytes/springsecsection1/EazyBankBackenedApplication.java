bda693c45120dad7543153f3c145ab8554bc9c1f
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-01-12 21:13:15 +0530

    created spring boot project

---
+++ Section1/springsecsection1/src/main/java/com/eazybytes/springsecsection1/EazyBankBackenedApplication.java
@@ -0,0 +1,13 @@
+package com.eazybytes.springsecsection1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazyBankBackenedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazyBankBackenedApplication.class, args);
+	}
+
+}
