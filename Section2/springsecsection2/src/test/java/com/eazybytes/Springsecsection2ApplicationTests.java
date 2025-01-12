02d6de8ffff4708a8a542ae0f65d9211b8ddb68e
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-01-13 00:23:40 +0530

    updated test class name

--- Section2/springsecsection2/src/test/java/com/eazybytes/Springsecsection2ApplicationTests.java
+++ Section2/springsecsection2/src/test/java/com/eazybytes/Springsecsection2ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Springsecsection2ApplicationTests {
+class EazyBankBackenedApplicationTests {
 
 	@Test
 	void contextLoads() {
