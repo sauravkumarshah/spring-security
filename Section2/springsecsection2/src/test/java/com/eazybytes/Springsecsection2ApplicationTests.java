60e0803a52fad03720421880f1cd7e07b26487dd
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-01-12 23:26:57 +0530

    created fresh spring boot project for section 2

---
+++ Section2/springsecsection2/src/test/java/com/eazybytes/Springsecsection2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybytes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springsecsection2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
