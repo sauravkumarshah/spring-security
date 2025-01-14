edf275c33e0419f17fc5cc1b7895693d0f9b1160
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-01-14 17:02:01 +0530

    whitelisted register endpoint and disabled csrf protection since due to csrf protection post requests are not allowed.

--- Section4/springsecsection4/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ Section4/springsecsection4/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -22,9 +22,11 @@ public class ProjectSecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         /*http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());*/
         /*http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());*/
-        http.authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
-                .requestMatchers("/notices", "/contact", "/error").permitAll());
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests((requests) -> requests
+            .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
+            .requestMatchers("/notices", "/contact", "/error", "/register").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
