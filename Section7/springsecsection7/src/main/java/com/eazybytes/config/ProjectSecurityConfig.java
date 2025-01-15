6e8bd9d98320157423cc0392ce0881df14d235db
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-01-16 00:37:19 +0530

    Added Custom Access Denied Handler

--- Section7/springsecsection7/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ Section7/springsecsection7/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.eazybytes.config;
 
+import com.eazybytes.exceptionhandling.CustomAccessDeniedHandler;
 import com.eazybytes.exceptionhandling.CustomBasicAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -32,6 +33,7 @@ public class ProjectSecurityConfig {
             .requestMatchers("/notices", "/contact", "/error", "/register").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
+        http.exceptionHandling(ehc -> ehc.accessDeniedHandler(new CustomAccessDeniedHandler()));
         return http.build();
     }
 //
