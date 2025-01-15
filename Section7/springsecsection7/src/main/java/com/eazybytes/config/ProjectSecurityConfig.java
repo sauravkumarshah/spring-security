6a72f784644c12c1cf0baa9390d495913a626bab
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-01-16 00:54:59 +0530

    added custome session timeout and invalidsession dummy url

--- Section7/springsecsection7/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ Section7/springsecsection7/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -26,11 +26,12 @@ public class ProjectSecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         /*http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());*/
         /*http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());*/
-        http.requiresChannel(rcc -> rcc.anyRequest().requiresInsecure()) // allow http
+        http.sessionManagement(smc -> smc.invalidSessionUrl("/invalidSession"))
+                .requiresChannel(rcc -> rcc.anyRequest().requiresInsecure()) // allow http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests((requests) -> requests
             .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
-            .requestMatchers("/notices", "/contact", "/error", "/register").permitAll());
+            .requestMatchers("/notices", "/contact", "/error", "/register", "/invalidSession").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
         http.exceptionHandling(ehc -> ehc.accessDeniedHandler(new CustomAccessDeniedHandler()));
--- Section7/springsecsection7/src/main/java/com/eazybytes/config/ProjectSecurityProdConfig.java
+++ Section7/springsecsection7/src/main/java/com/eazybytes/config/ProjectSecurityProdConfig.java
@@ -20,11 +20,12 @@ public class ProjectSecurityProdConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.requiresChannel(rcc -> rcc.anyRequest().requiresSecure()) // allow https
+        http.sessionManagement(smc -> smc.invalidSessionUrl("/invalidSession"))
+                .requiresChannel(rcc -> rcc.anyRequest().requiresSecure()) // allow https
                 .csrf(csrfConfig -> csrfConfig.disable())
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
-                        .requestMatchers("/notices", "/contact", "/error", "/register").permitAll());
+                        .requestMatchers("/notices", "/contact", "/error", "/register", "/invalidSession").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
         http.exceptionHandling(ehc -> ehc.accessDeniedHandler(new CustomAccessDeniedHandler()));
