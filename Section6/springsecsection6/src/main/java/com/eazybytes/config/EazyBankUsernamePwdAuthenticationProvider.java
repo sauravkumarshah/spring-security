d61f6ffc4e7c88aa9ebf5b6248eb7a49efc4cb26
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-01-15 23:45:51 +0530

    created default and prod profiles for different environment

--- Section6/springsecsection6/src/main/java/com/eazybytes/config/EazyBankUsernamePwdAuthenticationProvider.java
+++ Section6/springsecsection6/src/main/java/com/eazybytes/config/EazyBankUsernamePwdAuthenticationProvider.java
@@ -3,7 +3,6 @@ package com.eazybytes.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -13,9 +12,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("prod")
+@Profile("!prod")
 @RequiredArgsConstructor
-public class EazyBankProdUsernamePwdAuthenticationProvider implements AuthenticationProvider {
+public class EazyBankUsernamePwdAuthenticationProvider implements AuthenticationProvider {
 
     private final UserDetailsService userDetailsService;
     private final PasswordEncoder passwordEncoder;
@@ -25,12 +24,7 @@ public class EazyBankProdUsernamePwdAuthenticationProvider implements Authentica
         String username = authentication.getName();
         String pwd = authentication.getCredentials().toString();
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-        if (passwordEncoder.matches(pwd, userDetails.getPassword())) {
-            // Fetch Age details and perform validation to check if age >18
-            return new UsernamePasswordAuthenticationToken(username,pwd,userDetails.getAuthorities());
-        }else {
-            throw new BadCredentialsException("Invalid password!");
-        }
+        return new UsernamePasswordAuthenticationToken(username,pwd,userDetails.getAuthorities());
     }
 
     @Override
