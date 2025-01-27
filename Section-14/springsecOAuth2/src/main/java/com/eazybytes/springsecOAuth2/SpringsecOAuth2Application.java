fcefd46e4544e8a14c9db2cc53675d01b033283f
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-01-27 11:15:48 +0530

    created new spring boot project for OAuth2

---
+++ Section-14/springsecOAuth2/src/main/java/com/eazybytes/springsecOAuth2/SpringsecOAuth2Application.java
@@ -0,0 +1,13 @@
+package com.eazybytes.springsecOAuth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecOAuth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecOAuth2Application.class, args);
+	}
+
+}
---
+++ Section-14/springsecOAuth2/src/test/java/com/eazybytes/springsecOAuth2/SpringsecOAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybytes.springsecOAuth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecOAuth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
